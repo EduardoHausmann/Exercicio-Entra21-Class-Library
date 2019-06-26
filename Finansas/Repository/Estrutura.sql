0268ad49e39bbc5757596e1d404b0ee02d2ae8a3
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-06-26 11:04:27 -0300

    Feito o Codigo Do Banco De Dados

--- Finansas/Repository/Estrutura.sql
+++ Finansas/Repository/Estrutura.sql
@@ -1,1 +1,27 @@
-﻿
+﻿DROP TABLE contas_pagar;
+CREATE TABLE contas_pagar(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	valor DECIMAL(5,2),
+	tipo VARCHAR(50),
+	data_vencimento DATE
+);
+
+DROP TABLE contas_receber;
+CREATE TABLE contas_receber(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	valor DECIMAL(5,2),
+	valor_recebido DECIMAL(5,2),
+	data_recebimento DATE,
+	recebido BIT
+);
+
+DROP TABLE clientes;
+CREATE TABLE clientes(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	cpf VARCHAR(20),
+	data_nascimneto DATE,
+	rg VARCHAR(20)
+);
