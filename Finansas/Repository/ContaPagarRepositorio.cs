9c09af54e00f536a5894f410572f9a2a4c6f2f93
Eduardo Ehmke Hausmann <eduardohausmann1705@gmail.com>
2019-06-27 08:38:12 -0300

    Concluido o Repository do ContaPagar

--- Finansas/Repository/ContaPagarRepositorio.cs
+++ Finansas/Repository/ContaPagarRepositorio.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,115 @@ namespace Repository
 {
     public class ContaPagarRepositorio
     {
+        public string CadeiaConexaoPagar = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\Exercicio.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public void Inserir(ContaPagar contaPagar)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexaoPagar;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"INSERT INTO contas_pagar (nome, valor, tipo, dataVencimento) VALUES (@NOME, @VALOR, @TIPO, @DATAVENCIMENTO)";
+            comando.Parameters.AddWithValue("@NOME", contaPagar.Nome);
+            comando.Parameters.AddWithValue("@VALOR", contaPagar.Valor);
+            comando.Parameters.AddWithValue("@TIPO", contaPagar.Tipo);
+            comando.Parameters.AddWithValue("@DATAVENCIMENTO", contaPagar.DataVencimento);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+        }
+
+        public void Apagar(int id)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexaoPagar;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"DELETE FROM contas_pagar WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+        }
+
+         public List<ContaPagar> ObterTodos (string busca)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexaoPagar;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"SELECT * FROM contas_pagar WHERE nome LIKE @NOME";
+            busca = "%" + busca + "%";
+            comando.Parameters.AddWithValue("@NOME", busca);
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            conexao.Close();
+
+            List<ContaPagar> contasPagar = new List<ContaPagar>();
+            for (int i = 0; i < tabela.Rows.Count; i++)
+            {
+                DataRow linha = tabela.Rows[i];
+                ContaPagar contaPagar = new ContaPagar();
+                contaPagar.Id = Convert.ToInt32(linha["id"]);
+                contaPagar.Nome = linha["nome"].ToString();
+                contaPagar.Valor = Convert.ToDecimal(linha["valor"]);
+                contaPagar.Tipo = linha["tipo"].ToString();
+                contaPagar.DataVencimento = Convert.ToDateTime(linha["dataVencimento"]);
+                contasPagar.Add(contaPagar);
+            }
+
+            return contasPagar;
+        }
+
+        public ContaPagar ObterPeloId(int id)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexaoPagar;
+            conexao.Open();
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"SELECT * FROM contas_pagar WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+
+            if (tabela.Rows.Count ==0)
+            {
+                return null;
+            }
+
+            DataRow linha = tabela.Rows[0];
+            ContaPagar contaPagar = new ContaPagar();
+            contaPagar.Id = id;
+            contaPagar.Nome = linha["nome"].ToString();
+            contaPagar.Valor = Convert.ToDecimal(linha["valor"]);
+            contaPagar.Tipo = linha["tipo"].ToString();
+            contaPagar.DataVencimento = Convert.ToDateTime(linha["dataVencimento"]);
+            return contaPagar;
+        }
+
+        public void Alterar(ContaPagar contasPagar)
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = CadeiaConexaoPagar;
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"UPDATE contas_pagar SET nome = @NOME, valor = @VALOR, tipo = @TIPO, dataVencimento = @DATAVENCIMENTO WHERE id = @ID";
+            comando.Parameters.AddWithValue("@NOME", contasPagar.Nome);
+            comando.Parameters.AddWithValue("@VALOR", contasPagar.Valor);
+            comando.Parameters.AddWithValue("@TIPO", contasPagar.Tipo);
+            comando.Parameters.AddWithValue("@DATAVENCIMENTO", contasPagar.DataVencimento);
+            comando.Parameters.AddWithValue("@ID", contasPagar.Id);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+        }
     }
 }
