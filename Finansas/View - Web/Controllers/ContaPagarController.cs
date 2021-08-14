324942f52cff5ec792b79203f1db2482849846ab
EduardoHausmann <eduardohausmann1705@gmail.com>
2021-08-14 20:17:21 -0300

    Inseridos icones aos botões nas telas  de Cliente e feito o Controller de ContaPagar

--- Finansas/View - Web/Controllers/ContaPagarController.cs
+++ Finansas/View - Web/Controllers/ContaPagarController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,58 @@ namespace View___Web.Controllers
 {
     public class ContaPagarController : Controller
     {
-        // GET: ContaPagar
+        ContaPagarRepositorio repository = new ContaPagarRepositorio();
+
         public ActionResult Index()
         {
+            List<ContaPagar> contasPagar = repository.ObterTodos("");
+
+            ViewBag.ContasPagar = contasPagar;
+
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome, decimal valor, string tipo)
+        {
+            ContaPagar contaPagar = new ContaPagar();
+            contaPagar.Nome = nome;
+            contaPagar.Valor = valor;
+            contaPagar.Tipo = tipo;
+            int id = repository.Inserir(contaPagar);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            ContaPagar contaPagar = repository.ObterPeloId(id);
+
+            ViewBag.ContaPagar = contaPagar;
+
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, decimal valor, string tipo)
+        {
+            ContaPagar contaPagar = new ContaPagar();
+            contaPagar.Id = id;
+            contaPagar.Nome = nome;
+            contaPagar.Valor = valor;
+            contaPagar.Tipo = tipo;
+            repository.Alterar(contaPagar);
+
+            return RedirectToAction("Index");
         }
     }
 }
