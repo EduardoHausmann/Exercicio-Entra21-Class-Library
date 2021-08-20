2ac7f00972f9283d567c4eaedfcd960fca5d4ed8
EduardoHausmann <eduardohausmann1705@gmail.com>
2021-08-19 21:41:11 -0300

    Feito o Controller de Contas a Receber e suas respectivas Telas

--- Finansas/View - Web/Controllers/ContaReceberController.cs
+++ Finansas/View - Web/Controllers/ContaReceberController.cs
@@ -1,4 +1,5 @@
-﻿using Repository;
+﻿using Model;
+using Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,54 @@ namespace View___Web.Controllers
 
         public ActionResult Index()
         {
+            List<ContaReceber> contasReceber = repository.ObterTodos("");
+
+            ViewBag.ContasReceber = contasReceber;
+
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome, decimal valor, decimal valorRecebido)
+        {
+            ContaReceber contaReceber = new ContaReceber();
+            contaReceber.Nome = nome;
+            contaReceber.Valor = valor;
+            contaReceber.ValorRecebido = valorRecebido;
+            int id = repository.Inserir(contaReceber);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            ContaReceber contaReceber = repository.ObterPeloId(id);
+
+            ViewBag.ContaReceber = contaReceber;
+
+            return View();
+        }
+
+        public ActionResult Update(int id, string nome, decimal valor, decimal valorRecebido)
+        {
+            ContaReceber contaReceber = new ContaReceber();
+            contaReceber.Id = id;
+            contaReceber.Nome = nome;
+            contaReceber.Valor = valor;
+            contaReceber.ValorRecebido = valorRecebido;
+            repository.Alterar(contaReceber);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Apagar(int id)
+        {
+            repository.Apagar(id);
+            return RedirectToAction("Index");
         }
     }
 }
